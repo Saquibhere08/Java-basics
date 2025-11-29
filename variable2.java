9b767dc6968769aad8087caf95a1540eb47a0540
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-11-29 18:31:59 +0530

    Add files via upload

---
+++ variable2.java
@@ -0,0 +1,20 @@
+public class variable2 {
+    
+    public static void main (String[] args) {
+        
+        // Do not change the two lines below:
+        int myInt1 = 5;
+        int myInt2 = 7;
+
+        // Change myInt1 to 10 & myInt2 to 20:
+        myInt1=10;
+        myInt2=20;
+        
+
+        // Do not change the two lines below:
+        System.out.println("My Int 1 = " + myInt1);
+        System.out.println("My Int 2 = " + myInt2);
+    
+    }
+
+}

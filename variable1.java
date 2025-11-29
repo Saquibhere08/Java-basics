f9d5febe88e806e0195f98ecda907d749a8529ca
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-11-29 12:44:49 +0530

    Add files via upload

---
+++ variable1.java
@@ -0,0 +1,10 @@
+public class variable1 {
+    public static void main(String[] args) {
+        int x=10;
+        int y=50;
+
+        int z=x+y;
+        System.out.println(z);
+    }
+}
+//integer variable

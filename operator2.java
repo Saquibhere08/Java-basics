bf0ddf27fd49be74efe7bda4aa539300347a891c
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-11-30 22:43:59 +0530

    Add files via upload

---
+++ operator2.java
@@ -0,0 +1,19 @@
+public class operator2 {
+    public static void main(String[] args) {
+        int length=30;
+        int width=10;
+
+        int area;
+        int perimeter;
+
+        //formulas
+
+        area=length*width;
+        perimeter=2*(length+width);
+
+        //print
+        System.out.println("The area of the rectangle is: "+ area);
+        System.out.println("The perimeter of the rectangle is: "+ perimeter);
+    }
+}
+//Rectangle

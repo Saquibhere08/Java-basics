8e47601c00fb6412407eb10870a6a024fef31504
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-12-03 21:26:07 +0530

    Add files via upload

---
+++ string8.java
@@ -0,0 +1,11 @@
+public class string8 {
+    //escape sequences
+    public static void main(String[] args) {
+        System.out.println("\"Escape Sequence 1\"");
+        System.out.println("\nEscape Sequence 2");
+        System.out.println("\'Escape Sequence 3\'");
+        System.out.println("Escape \tSequence 2");
+
+    }
+}
+

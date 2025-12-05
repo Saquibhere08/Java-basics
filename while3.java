1cd2c2e7d15fed0a3fcfcfa5c2eaf33017853121
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-12-05 16:07:54 +0530

    Add files via upload

---
+++ while3.java
@@ -0,0 +1,15 @@
+public class while3 {
+    public static void main(String[] args) {
+        int i=0;
+        while(i<3){
+            if(i>=3){ //reverse logic
+                break;
+            }
+            System.out.println(++i);
+        }
+
+
+
+
+    }
+}

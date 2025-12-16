009811b6752c92950b72714c6fae13b8d51eff28
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-12-16 16:54:25 +0530

    Add files via upload

---
+++ evenOrodd.java
@@ -0,0 +1,34 @@
+public class evenOrodd {
+    public static String evenOrOdd(int number) {
+        // ----------------------------------------
+        // COMPLETE THE CODE FOR THE METHOD HERE:
+        //
+        // Given an integer, return "even" if the
+        // number is even, or "odd" if the number
+        // is odd.
+        //
+        // Use % 2 to determine even/odd.
+        // ----------------------------------------
+        if(number % 2==0){
+            return "even";
+            
+        }else{
+            return "odd";
+        }
+    }
+    
+
+    
+    
+    public static void main(String[] args) {
+        
+        // Do not change the code in the main method
+        
+        System.out.println("4 is " + evenOrOdd(4));    // even
+        System.out.println("7 is " + evenOrOdd(7));    // odd
+        System.out.println("0 is " + evenOrOdd(0));    // even
+        System.out.println("-6 is " + evenOrOdd(-6));  // even
+        System.out.println("-3 is " + evenOrOdd(-3));  // odd
+        
+    }
+}

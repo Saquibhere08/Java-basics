8c686fbcbe54871ee5620156a8e391df82447f14
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-12-16 22:47:18 +0530

    Add files via upload

---
+++ maps1.java
@@ -0,0 +1,17 @@
+import java.util.Map;
+import java.util.HashMap;
+
+
+public class maps1 {
+    public static void main(String[] args) {
+        
+       Map<String,Integer> ages=new HashMap<>();
+       ages.put("Saquib",24); 
+       ages.put("Nazeeb",19); 
+       ages.put("Shubham",23); 
+       ages.put("Dristant",23); 
+       ages.put("Shanan",20); 
+
+       System.out.println(ages);
+    }
+}
---
+++ set1.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.TreeSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+public class set1 {
+    public static void main(String[] args) {
+        Set<Integer> mySet=new LinkedHashSet<>();
+        mySet.remove(5);
+        mySet.conatins(1);
+        mySet.size();
+        mySet.isEmpty();
+        mySet.clear();
+
+    }
+}

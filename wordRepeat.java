ae1871e1f535668835f975720809b9e15161b643
Saquib Bin Halim <halimsaquib99@gmail.com>
2025-12-24 11:18:04 +0530

    Add files via upload

---
+++ wordRepeat.java
@@ -0,0 +1,36 @@
+public class wordRepeat {
+     public static String repeatWord(String word, int count) {
+        // ----------------------------------------
+        // COMPLETE THE CODE FOR THE METHOD HERE:
+        //
+        // Given a string and a non-negative integer,
+        // return the string repeated that many times.
+        //
+        // If count is 0 or the string is empty,
+        // return an empty string.
+        // ----------------------------------------
+    
+        String result= "";
+        for(int i=0;i<count;i++){
+            result+=word;
+            
+        }
+        return result;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+
+        // Do not change the code in the main method
+
+        System.out.println("\"hi\" * 3 → " + repeatWord("hi", 3));       // hihihi
+        System.out.println("\"abc\" * 2 → " + repeatWord("abc", 2));     // abcabc
+        System.out.println("\"hello\" * 0 → " + repeatWord("hello", 0)); // (empty)
+        System.out.println("\"a\" * 5 → " + repeatWord("a", 5));         // aaaaa
+        System.out.println("\"\" * 4 → " + repeatWord("", 4));           // (empty)
+
+    }
+}

65622139c4ca844c46d8a1442b64123dce8864eb
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-01-09 18:01:44 +0530

    Add files via upload

---
+++ sumDigits.java
@@ -0,0 +1,45 @@
+public class sumDigits{
+    
+        public static int sumDigits(String text) {
+        // --------------------------------------------------
+        // COMPLETE THE CODE FOR THE METHOD HERE:
+        //
+        // This method takes a string of digits and returns
+        // the sum of all digits in the string.
+        //
+        // It uses Character.getNumericValue() to convert
+        // characters to integers and adds them together.
+        // --------------------------------------------------
+        int total=0;
+        for(int i=0;i<text.length();i++){
+            char digitChar=text.charAt(i);
+            
+            int digit=Character.getNumericValue(digitChar);
+            
+            total+=digit;
+        }
+        return total;
+        
+    
+
+
+
+
+
+    public static void main(String[] args) {
+    
+        // Do not change the code in the main method
+    
+        System.out.println("\"12345\" → " + sumDigits("12345"));   // 15
+        System.out.println("\"5\" → " + sumDigits("5"));           // 5
+        System.out.println("\"000\" → " + sumDigits("000"));       // 0
+        System.out.println("\"\" → " + sumDigits(""));             // 0
+        System.out.println("\"908172\" → " + sumDigits("908172")); // 27
+    }
+    }
+
+
+
+
+
+}
